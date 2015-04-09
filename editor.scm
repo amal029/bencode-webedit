a39ad8fa09748a1df4ead3a57b187d9352cb4a33
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-04-09 12:22:48 +1200

    added getting the file name

--- editor.scm
+++ editor.scm
@@ -4,26 +4,41 @@
 (enable-sxml #t)
 (literal-script/style? #t)
 
+(page-css "editor.css")
+
+;;; The path name of the file
+(define file-name "")
+
 (define-page
   (main-page-path)
   (lambda ()
     (set-page-title! "Bencode Editor")
-    (ajax "mo" 'ofile 'click
+    (ajax "mo" 'myInput 'change
     	  (lambda ()
-    	    (let ((fname ($ 'myInput)))
-	      `(b ,fname)))
-	  target: "file-input")
+	    (set! file-name ($ 'myInput as-string))
+	    '())
+	  arguments:'((myInput . "$('#myInput').val()"))
+	  success: "$('#target').submit();")
+    (ajax "submit-handler" 'target 'submit
+	  (lambda ()
+	    `(b ,file-name)))
     `((ul (@ (class "menu"))
 	  (li (a (@ (href "#")) "File")
 	      (ul 
-	       (div (@ (id "ofile")
-		       (onclick 
-			"document.getElementById('myInput').click();"))
-		    (input (@ (type "file")
-			      (id "myInput")
-			      (style "display:none")))
-		    (li (a (@ (id "mo") (href "#")) 
-			   "Open")))
+	       (div
+		(@ (id "ofile")
+		   (onclick 
+		    "document.getElementById('myInput').click();"))
+		(form (@ (id "target")
+			 (action "/ajax/submit-handler")
+			 (enctype "multipart/form-data")
+			 (method "post"))
+		 (input (@ (type "file")
+			   (id "myInput")
+			   (value "Go")
+			   (style "display:none"))))
+		(li (a (@ (id "mo") (href "#")) 
+		       "Open")))
 	       (li (a (@ (id "mor") (href "#")) "Open Recent"))))
 	  (li (a (@ (href "#")) "Edit")))
       (div (@ (id "file-input")))))
